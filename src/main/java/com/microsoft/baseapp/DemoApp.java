865d354ca26a26eda38aa6c4abd12d866b9f5711
Itye <itye@ityer.guest.corp.microsoft.com>
2020-12-10 17:50:31 +0200

    Added DI

---
+++ src/main/java/com/microsoft/baseapp/DemoApp.java
@@ -0,0 +1,29 @@
+package com.microsoft.baseapp;
+
+import com.microsoft.baseapp.config.IConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApp {
+
+	@Autowired
+	private IConfigService myConfig;
+
+	@Value("${application.message:Not configured by a Spring Cloud Server}")
+	private String message;
+
+	@GetMapping("/getsecret")
+	public String getsecret() {
+		return this.myConfig.getSecret();
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return message;
+	}
+}
--- src/main/java/com/microsoft/baseapp/Main.java
+++ src/main/java/com/microsoft/baseapp/Main.java
@@ -1,24 +1,13 @@
 package com.microsoft.baseapp;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
 
-@SpringBootApplication
-@RestController
+import org.springframework.boot.SpringApplication;
+
 public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-    }
 
-    @Value("${application.message:Not configured by a Spring Cloud Server}")
-    private String message;
+        SpringApplication.run(DemoApp.class, args);
 
-    @GetMapping("/hello")
-    public String hello() {
-        return message + '\n';
     }
 
 }
